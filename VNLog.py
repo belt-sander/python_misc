d3879c71d6b0d6663258f57d921f8a8cc3a8a4cf
Sander Marques <sander@mapper.ai>
2019-06-07 20:54:23 -0700

    Function additions

--- VNLog.py
+++ VNLog.py
@@ -26,15 +26,37 @@ def parse_args():
                             action='store_true',
                             required=False,
                             help='print terminal output from sensor')
-    arg_parser.add_argument('-d',
-                            '--data_header',
+    arg_parser.add_argument('-a',
+                            '--ascii_group',
                             required=False,
                             default='$VNINS',
                             help='requested VectorNav message (default: $VNINS)')
+    arg_parser.add_argument('-f',
+                            '--data_freq',
+                            required=False,
+                            default='10',
+                            #type=int,
+                            help='requested VectorNav data frequency int (default: 40) (40hz)')
+
     return arg_parser.parse_args()
 
 def vn_message_type():
+    args = parse_args()
 
+    if args.ascii_group =='$VNINS':
+        print('you r1ght bruh')
+    else:
+        print('you lame AF')
+
+def sensor_config():
+    args = parse_args()
+    ### set data update rate    
+
+    freqConfigHeader = ('$VNWRG,07,').encode()
+    freqValue = (args.data_freq)
+    freqCRC = (',1*XX').encode()
+    newLineCarRet = ('\r \n').encode()        
+    return freqConfigHeader, freqValue, freqCRC, newLineCarRet
 
 def main():
     args = parse_args()
@@ -48,41 +70,55 @@ def main():
     ser.timeout = 5                         #non-block read
     ser.xonxoff = False                     #disable software flow control
     ser.rtscts = False                      #disable hardware (RTS/CTS) flow control
-    ser.dsrdtr = False                      #disable hardware (DSR/DTR) flow control
+    ser.dsrdtr = False                      #disable hardware (DSR/DTR) flow control    
 
     try: 
         ser.open()
-
+        ser.flush()
+        
     except Exception, e:
         print "error open serial port: " + str(e)
         exit()
 
-        ser.flush()
+    ### data frequency from sensor. limited to 50hz unless baudrate is changed! 
+    # freqConfigHeader = ('$VNWRG,07,').encode()
+    # freqValue = (args.data_freq)
+    # freqCRC = (',1*XX').encode()
+    # newLineCarRet = ('\r \n').encode()        
+    
+    freqConfigHeader, freqValue, freqCRC, newLineCarRet = sensor_config()
+
+    ser.write(freqConfigHeader)
+    ser.write(freqValue)
+    ser.write(freqCRC)
+    ser.write(newLineCarRet)
 
     if ser.isOpen():
+        file = open(os.path.join(args.output,'vectornav_data.txt'), 'w') #w for write as opposed to append
         while True:
             try:
                 line = ser.readline() # unsure if needed -> .decode('utf-8')
                 if line:
                     if '$VNINS' in line:
-                        data = line[10:].strip() #.split(',')
+                        data = line[7:].strip() #.split(',')
                         if args.print_output:
                             print(data)
                         else:
                             print('data is being written to file')
-                        with open(os.path.join(args.output,'vectornav_data.txt'), 'a') as d: #w for write as opposed to append
-                            d.write(data+'\n')
+                        file.write(data+'\n')
                 if line =='!':
                     ser.close()
                     break
             
             except:
-                print('Keyboard Interrupt')
+                print('')
+                print('user input ended logging')
+                print('')
                 ser.close()
                 break
 
     else:
-        print("Can not open serial port")
+        print("cannot open serial port")
 
 if __name__=='__main__':
     main()
