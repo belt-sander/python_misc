9cf6eb2472059a6f70405a1db5e6f7adb6012df6
Sander Marques <sander@mapper.ai>
2019-08-19 22:42:15 -0700

    Added options for printing data or replaying data

--- PandaParser.py
+++ PandaParser.py
@@ -5,11 +5,10 @@
 
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-import binascii
 import time
 from panda import Panda
 import struct
+import sys
 
 def parse_args():
 	arg_parser = argparse.ArgumentParser(description='PandaLogger.py data parser')
@@ -19,51 +18,45 @@ def parse_args():
 							help='output file from PandaLogger.py')
 	arg_parser.add_argument('-t',
 							'--testMode',
-							required=True,
+							required=False,
+							default=False,
 							help='set to <True> if ack is required')	
 	arg_parser.add_argument('-m',
 							'--mask',
 							required=False,
+							default='0x800',
 							help='play back data only on this identifier <decimal>')
-	arg_parser.add_argument('-m2',
-							'--mask2',
-							required=False,
-							help='play back data only on this identifier <decimal>')
+	arg_parser.add_argument('-r',
+							'--replay',
+							required=True,
+							default=False,
+							help='play back physical data via CAN')
 	arg_parser.add_argument('-s',
 							'--sleep',
 							required=True,
 							type=float,
 							help='sleep time (ms) between playing messages back')
-
 	return arg_parser.parse_args()
 
 def main():
 	args = parse_args()
 
-	try:
-		print("Trying to connect to Panda over USB...")
-		p = Panda()
-	except AssertionError:
-		print("USB connection failed.")
+	if args.replay == 'True':
 		try:
-			p = Panda("WIFI")
-		except:
-			print("WiFi connection timed out. Please make sure your Panda is connected and try again.")
+			print("Trying to connect to Panda over USB...")
+			p = Panda()
+		except AssertionError:
+			print("USB connection failed.")
 			sys.exit(0)
 
-	# turn off safety mode
-	if args.testMode == "True":
-		p.set_safety_mode(p.SAFETY_ALLOUTPUT)
-	elif args.testMode == "False":
-		p.set_safety_mode(p.SAFETY_NOOUTPUT)
-	else:
-		print("incorrect testMode arguement")
-		sys.exit("exiting...")
-
-	# set bus speeds. nominally 500kbps
-	# p.set_can_speed_kbps(0,1000)	
-	# p.set_can_speed_kbps(1,1000)
-	# p.set_can_speed_kbps(2,1000)	
+		# turn off CAN tx safety mode
+		if args.testMode == '	True':
+			p.set_safety_mode(p.SAFETY_ALLOUTPUT)
+		elif args.testMode == 'False':
+			p.set_safety_mode(p.SAFETY_NOOUTPUT)
+		else:
+			print("incorrect testMode arguement")
+			sys.exit("exiting...")
 
 	canData = np.genfromtxt(args.input, skip_header=1, delimiter=',', dtype=str	)
 	print("data has been imported")
@@ -71,16 +64,9 @@ def main():
 	numCanPackets = np.size(canData,0)
 	print("num can samples: ", numCanPackets)
 
-	print("args mask", (args.mask))
-	maskint = int(args.mask, 0)
-
-	print("args mask 2", (args.mask2))
-	maskint2 = int(args.mask2, 0)
-
-	# busNum = canData[:,0]
-	# messIden = canData[:,1]
-	# data = canData[:,2]
-	# length = canData[:,3]
+	if args.mask is not None:
+		print("args mask", (args.mask))
+		maskint = int(args.mask, 0)
 
 	for i, row in enumerate (canData):
 		busNum = row[0]
@@ -93,22 +79,26 @@ def main():
 		lengthInt = int(length,0)
 		dataInt = int(data,0)
 
-		if messIdenInt == maskint:
-			dataStructOut = struct.pack('>Q',dataInt)
-			p.can_send(messIdenInt,dataStructOut,busNumInt)
-			print([(busNumInt), (messIdenInt), (data), (lengthInt)])
-			time.sleep(args.sleep)
-		elif messIdenInt == maskint2:
-			dataStructOut = struct.pack('>I',dataInt)
-			p.can_send(messIdenInt,dataStructOut,busNumInt)
-			print([(busNumInt), (messIdenInt), (data), (lengthInt)])
-			time.sleep(args.sleep)
+		if args.replay == 'True':	
+			if messIdenInt == maskint:
+				dataStructOut = struct.pack('>Q',dataInt) # '>Q' argument == big endian long struct format
+				p.can_send(messIdenInt,dataStructOut,busNumInt)
+				print([(busNumInt), (messIden), (data), (lengthInt)])
+				time.sleep(args.sleep)
+			elif args.mask is None:
+				print([(busNumInt), (messIden), (data), (lengthInt)])
+				time.sleep(args.sleep)
+		
+		elif args.replay == 'False':
+			if maskint == 0x800:
+				print([(busNumInt), (messIden), (data), (lengthInt)])
+				time.sleep(args.sleep)
+			elif messIdenInt == maskint:
+				print([(busNumInt), (messIden), (data), (lengthInt)])
+				time.sleep(args.sleep)				
 
-		elif args.mask is None:
-			print([(busNumInt), (messIdenInt), (data), (lengthInt)])
-			time.sleep(args.sleep)
-
-
+	# re-enable safety mode
+	p.set_safety_mode(p.SAFETY_NOOUTPUT)
 
 if __name__=='__main__':
 	main()
