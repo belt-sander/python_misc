feb4fcfc9986ceae8c6bad6c810f54d72e529912
Sander Marques <sander@mapper.ai>
2019-08-27 20:56:07 -0700

    Added crc check stuff

--- PandaParser.py
+++ PandaParser.py
@@ -81,24 +81,56 @@ def main():
 
 		if args.replay == 'True':	
 			if maskint == 0x800:
-				print([(busNumInt), (messIden), (data), (lengthInt)])
+				dataStructOut = struct.pack('>Q',dataInt) # '>Q' argument == big endian long struct format
+				# print([(busNumInt), (messIden), (data), (lengthInt)])
+				p.can_send(messIdenInt,dataStructOut,busNumInt)
 				time.sleep(args.sleep)
 			elif messIdenInt == maskint:
 				dataStructOut = struct.pack('>Q',dataInt) # '>Q' argument == big endian long struct format
 				p.can_send(messIdenInt,dataStructOut,busNumInt)
-				print([(busNumInt), (messIden), (data), (lengthInt)])
+				# print([(busNumInt), (messIden), (data), (lengthInt)])								
 				time.sleep(args.sleep)
 		
 		elif args.replay == 'False':
 			if maskint == 0x800:
+				dataStructOut = struct.pack('>Q',dataInt) # '>Q' argument == big endian long struct format
 				print([(busNumInt), (messIden), (data), (lengthInt)])
 				time.sleep(args.sleep)
+
 			elif messIdenInt == maskint:
-				print([(busNumInt), (messIden), (data), (lengthInt)])
+				dataStructOut = struct.pack('>Q',dataInt) # '>Q' argument == big endian 8 byte unsigned // '>I' argument == big endian 4 byte unsigned
+				# print([(busNumInt), (messIden), (data), (lengthInt)])
+				
+				###
+				# data manipulation (2 byte chunks)
+				###
+				data2byteOut = struct.unpack(b'4H',dataStructOut)
+				print('wh spd? : ', data2byteOut[2])
+				
+				###
+				# crc test // CORRECT FOR 0x488 ((byte0 + byte1 + byte2 + id + len)%256)
+				# crc test // CORRECT FOR 0x370 ((byte0 + byte1 + byte2 + byte3 + byte4 + byte5 + byte6 + id + len + -5)%256)
+				# crc test // CORRECT FOR 0x175 ((byte0 + byte1 + byte2 + byte3 + byte4 + byte5 + byte6 + id + len + -7)%256)
+				###
+
+				# mysteryFactor = -5
+				# dataSum = mysteryFactor + messIdenInt + lengthInt + ord(dataStructOut[0]) + ord(dataStructOut[1]) + ord(dataStructOut[2]) + ord(dataStructOut[3]) + ord(dataStructOut[4]) + ord(dataStructOut[5]) + ord(dataStructOut[6])
+				# crc = dataSum%256
+				# if crc != ord(dataStructOut[7]):
+				# 	print('wrong crc y0!!!!')
+				# 	print('sum: ', dataSum)
+				# 	print('this is calculated crc: ', crc, 'this is the last byte: ', ord(dataStructOut[7]))
+				# 	print('error: ', crc - ord(dataStructOut[7]))
+				# 	print([(busNumInt), (messIden), (data), (lengthInt)])
+
 				time.sleep(args.sleep)				
 
 	# re-enable safety mode
-	p.set_safety_mode(p.SAFETY_NOOUTPUT)
+	if args.replay == 'True':
+		p.set_safety_mode(p.SAFETY_NOOUTPUT)
+		print('panda saftey mode re-enabled')
+	else:
+		print('playback finished...')
 
 if __name__=='__main__':
 	main()
