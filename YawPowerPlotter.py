5b5d8242cfa38ff6d4696a55b8cbc376d443355c
Sander Marques <sander@mapper.ai>
2019-09-01 22:28:27 -0700

    changed formatting for new text file import type

--- YawPowerPlotter.py
+++ YawPowerPlotter.py
@@ -16,7 +16,7 @@ def parse_args():
 def main():
 	args = parse_args()
 	
-	data = np.genfromtxt(args.input, skip_header=6, delimiter='	')
+	data = np.genfromtxt(args.input, skip_header=125, skip_footer=6000, delimiter='	')
 	print("data has been imported")	
 
 	dataRows = np.size(data,0)
@@ -24,32 +24,32 @@ def main():
 
 	# accel group
 	sysTime = data[:,0]
-	vnAccX = data[:,1]
-	izzeAccX = data[:,2]
-	vnAccY = data[:, 3]
-	izzeAccY = data[:, 4]
-	vnAccZ = data[:, 5]
-	izzeAccZ = data[:, 6]	
+	vnAccX = data[:,2]
+	izzeAccX = data[:,3]
+	vnAccY = data[:, 4]
+	izzeAccY = data[:, 5]
+	vnAccZ = data[:, 6]
+	izzeAccZ = data[:, 7]	
 	
 	# position group
-	vnLat = data[:,8]
-	vnLong = data[:,9]
+	vnLat = data[:,9]
+	vnLong = data[:,10]
 
 	# orientation group
-	vnPitch = data[:, 10]
-	vnRoll = data[:, 11]
-	vnYaw = data[:, 12]
+	vnPitch = data[:, 11]*(180/np.pi)
+	vnRoll = data[:, 12]*(180/np.pi)
+	vnYaw = data[:, 13]*(180/np.pi)
 
 	#eTesta alt (garbage data)
-	etAlt = data[:, 13]
+	etAlt = data[:, 14]
 
 	# engine data
-	rpm = data[:, 30]
+	rpm = data[:, 26]
 
 	# global vel
-	vnVelGlobalX = data[:, 40]
-	vnVelGlobalY = data[:, 41]
-	vnVelGlobalZ = data[:, 42]
+	vnVelGlobalX = data[:, 34]
+	vnVelGlobalY = data[:, 35]
+	vnVelGlobalZ = data[:, 36]
 
 	vnRelVelX = np.zeros((len(data),1))
 	vnRelVelY = np.zeros((len(data),1))
@@ -62,44 +62,58 @@ def main():
 	# 	# print(vnTime)
 	# 	vnTimeOut[i,:] = vnTime		
 
+	print(sysTime)
+
 	for i, row in enumerate(data):
-		forVnYaw = row[12]
-		forVnVelX = row[40]
-		forVnVelY = row[41]
-		forVnVelZ = row[42]
+		forVnYaw = row[13]
+		forVnVelX = row[34]
+		forVnVelY = row[35]
+		forVnVelZ = row[36]
 		vnRelVelX[i,:] = (np.sin(forVnYaw)*forVnVelY + np.cos(forVnYaw)*forVnVelX)
 		# print(vnRelVelX)
 		vnRelVelY[i,:] = (np.cos(forVnYaw)*forVnVelY + (-np.sin(forVnYaw)*forVnVelX))
 		# print(vnRelVelY)
-		# vnSpd[i,:] = np.sqrt((forVnVelX^2)+(forVnVelY^2)+(forVnVelZ^2))
-		# print(vnSpd)
+		vnSpd[i,:] = np.sqrt((forVnVelX**2)+(forVnVelY**2)+(forVnVelZ**2))
 		zero[i,:] = 0
 
-	plt.figure(1)	
-	plt.subplot(411)
-	plt.title('lat / long (dd)')
-	plt.plot(vnLat, vnLong, color='blue')
+	print(vnSpd)
 
-	plt.subplot(412)
-	plt.title('roll and pitch (rad)')
+	plt.figure(1)	
+	plt.subplot(211)
+	plt.title('lat / long (dd)')
+	plt.plot(sysTime, vnLat, label='lat')
+	# plt.xlim(2000, 2700)
+
+	plt.subplot(212)
+	plt.plot(sysTime, vnLong, label='long')
+	# plt.xlim(2000, 2700)
+
+	plt.figure(2)
+	plt.subplot(311)
+	plt.title('roll and pitch (deg)')
 	plt.plot(sysTime, vnRoll, color='red', label='roll')
 	plt.plot(sysTime, vnPitch, color='green', label='pitch')
 	plt.legend()
+	plt.ylim(-20,20)
+	plt.xlim(2400,2500)
 
-	plt.subplot(413)
+	plt.subplot(312)
 	plt.title('body velocity (m/s)')
 	plt.plot(sysTime, vnRelVelX, color='red', label='rel vel x')
 	plt.plot(sysTime, vnRelVelY, color='magenta', label='rel vel y')
 	plt.plot(sysTime, vnVelGlobalZ, color='blue', label='global vel z')
+	plt.plot(sysTime, vnSpd, color='green', label='speed (m/s)')	
 	plt.plot(sysTime, zero, color='black')
 	plt.legend()
+	plt.xlim(2400,2500)
 
-	plt.subplot(414)
+	plt.subplot(313)
 	plt.title('global (NED) velocity (m/s)')
 	plt.plot(sysTime, vnVelGlobalX, color='pink', label='global vel x')
 	plt.plot(sysTime, vnVelGlobalY, color='blue', label='global vel y')
 	plt.plot(sysTime, vnVelGlobalZ, color='green', label='global vel z')
 	plt.legend()
+	plt.xlim(2450,2500)
 
 	plt.show()
 
