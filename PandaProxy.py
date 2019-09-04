f57d865e5908cb16718cafee2eaf522e6371cdf8
Sander Marques <sander@mapper.ai>
2019-09-03 20:18:41 -0700

    print formatting and stuff

--- PandaProxy.py
+++ PandaProxy.py
@@ -7,6 +7,7 @@ import argparse
 import sys, traceback
 import time
 import binascii
+import struct
 
 def parse_args():
 	arg_parser = argparse.ArgumentParser(description='recieved can data forwarder from bus 0 to bus 1')
@@ -22,13 +23,13 @@ def main():
 	try:
 		print("Trying to connect to Panda over USB...")
 		p = Panda()
+		
+		p.can_clear(0xffff)
+		print('can ringbuffer cleared')
+		
 	except AssertionError:
 		print("USB connection failed.")
-		try:
-			p = Panda("WIFI")
-		except:
-			print("WiFi connection timed out. Please make sure your Panda is connected and try again.")
-			sys.exit(0)
+		sys.exit(0)
 
 	# turn off safety mode
 	if args.testMode == "True":
@@ -45,8 +46,8 @@ def main():
 
 	### can forwarder settings ###
 	# p.set_can_forwarding(0,1)
-	# p.set_can_forwarding(1,0)
-	# print('forwarder enabled 0->1 1->0')
+	p.set_can_forwarding(1,0)
+	print('forwarder enabled can_1 -> can_0')
 
 	### data format below ###
 	# ('input data: ', [(1638, 59728, bytearray(b'\xff\xff\x00\x00\x00\x00\x00\x00'), 0)])
@@ -55,6 +56,7 @@ def main():
 	# ('dat: ', '\xff\xff\x00\x00\x00\x00\x00\x00')
 
 	count = 0
+	countData2 = 0
 	newSend = False
 	newSendData2 = False
 
@@ -64,25 +66,31 @@ def main():
 			if src == 0 and address == 0x666:
 				newSend = True				
 				if newSend == True:
-					print('input data: ', inputData)
-					print('converted output: ', [str(src), str(hex(address)), "0x" + binascii.hexlify(dat), len(dat)])		
-					print('addr: ', address)
-					print('dat: ', str(dat))
-					print('send state: ', newSend)					
+					print'input data: ', inputData
+					print'converted output: ', [str(src), str(hex(address)), "0x" + binascii.hexlify(dat), len(dat)]		
+					print'addr: ', address
+					print'send state: ', newSend					
 					count = count + 1
-					print('counter: ', count)
-					p.can_send(address, str(dat),1)
+					print'counter: ', count
+
+					### simple test moving byte 0,1 to positon 6,7 ###
+					db = '0x' + binascii.hexlify(dat)
+					dbm = '0x'+'00'+'00'+'00'+'00'+'00'+'00'+db[:6][2:]
+					dbmi = int(dbm,0)
+					dbmip = struct.pack('>Q', dbmi)
+					p.can_send(address, dbmip,1)
 
 			if src == 0 and address == 0x034:
 				newSendData2 = True
 				if newSendData2 == True:
-					print('input data: ', inputData)
-					print('converted output: ', [str(src), str(hex(address)), "0x" + binascii.hexlify(dat), len(dat)])		
-					print('addr: ', address)
-					print('dat: ', str(dat))
-					print('send state: ', newSendData2)					
-					count = count + 1
-					print('counter: ', count)
+					print'input data: ', inputData
+					print'converted output: ', [str(src), str(hex(address)), "0x" + binascii.hexlify(dat), len(dat)]		
+					print'addr: ', address
+					print'send state: ', newSendData2					
+					countData2 = countData2 + 1
+					print'counter: ', count
+
+					### just a basic forwarder ###
 					p.can_send(address, str(dat),1)					
 
 if __name__ == "__main__":
