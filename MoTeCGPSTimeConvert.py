58e0e561310b8eaa5328c652cb64b746322dd488
Sander Marques <sander@mapper.ai>
2019-09-24 19:54:42 -0700

    changed column identifiers

--- MoTeCGPSTimeConvert.py
+++ MoTeCGPSTimeConvert.py
@@ -41,71 +41,80 @@ def main():
     print(timeOffset)
     print("")
 
-    inputData = np.genfromtxt(args.input_csv, skip_header=16, delimiter=',')
+    inputData = np.genfromtxt(args.input_csv, skip_header=17, delimiter=',')
     print("data has been imported")
     print("")
 
     gpsGenPosix = np.zeros((len(inputData),1))
-    gpsLat = inputData[:,9]
-    gpsLong = inputData[:,10]
+    sysTime = inputData[:,0]
+    gpsTimeTest = inputData[:,77]
+    gpsLat = inputData[:,19]
+    gpsLong = inputData[:,20]
     avgSpd = inputData[:,1]
-    brakeState = inputData[:,2]
-    wheelSpeedFL = inputData[:,18]
-    wheelSpeedFR = inputData[:,19]
-    wheelSpeedRL = inputData[:,20]
-    wheelSpeedRR = inputData[:,21]
-    steerWheelAngle = inputData[:,22]
-    wheelMoveState = inputData[:,23]
-    brakePresF = inputData[:,24]
-    brakePresR = inputData[:,25]
+    brakeState = inputData[:,7]
+    wheelSpeedFL = inputData[:,56]
+    wheelSpeedFR = inputData[:,57]
+    wheelSpeedRL = inputData[:,58]
+    wheelSpeedRR = inputData[:,59]
+    steerWheelAngle = inputData[:,62]
+    wheelMoveState = inputData[:,66]
+    brakePresF = inputData[:,67]
+    brakePresR = inputData[:,68]
     avgBrakePres = brakePresR+brakePresF
-    vnYaw = inputData[:,3]
-    vnPitch = inputData[:,4]
-    vnRoll = inputData[:,5]
-    vnVelX = inputData[:,6]
-    vnVelY = inputData[:,7]
-    vnVelZ = inputData[:,8]
-    vnAccelX = inputData[:,11]
-    vnAccelY = inputData[:,12]
-    vnAccelZ = inputData[:,13]
-    vnGyroX = inputData[:,14]
-    vnGyroY = inputData[:,15]
-    vnGyroZ = inputData[:,16]
-    rgLat = inputData[:,28]
-    rgLong = inputData[:,29]
+    vnYaw = inputData[:,13]
+    vnPitch = inputData[:,14]
+    vnRoll = inputData[:,15]
+    vnVelX = inputData[:,16]
+    vnVelY = inputData[:,17]
+    vnVelZ = inputData[:,18]
+    vnAccelX = inputData[:,21]
+    vnAccelY = inputData[:,22]
+    vnAccelZ = inputData[:,23]
+    vnGyroX = inputData[:,24]
+    vnGyroY = inputData[:,25]
+    vnGyroZ = inputData[:,26]
+    rgLat = inputData[:,85]
+    rgLong = inputData[:,86]
+    wheelOdoRR = inputData[:,91]
+    speedOBDResponse = inputData[:,49]
 
     print("iterating through data...")
     print("")
     for i, row in enumerate(inputData):
-        gpsSec = row[26]
+        gpsSec = row[77]
         gpsGenPosix[i,: ] = gpsSec + timeOffset
         # print("new posix time:")
         # print(gpsGenPosix)
 
-    dataOutput = np.column_stack((gpsGenPosix,avgSpd,brakeState,wheelSpeedFL,wheelSpeedFR,wheelSpeedRL,wheelSpeedRR,steerWheelAngle,wheelMoveState,avgBrakePres,vnYaw,vnPitch,vnRoll,vnAccelX,vnAccelY,vnAccelZ,vnGyroX,vnGyroY,vnGyroZ,gpsLat,gpsLong,rgLat,rgLong, vnVelX, vnVelY, vnVelZ))
-    np.savetxt(args.output, dataOutput, fmt='%.8f', delimiter=' ', header="# gpsGenPosix(s),avgSpd(mph),brakeState(unitless),wheelSpeedFL(mph),wheelSpeedFR(mph),wheelSpeedRL(mph),wheelSpeedRR(mph),steerWheelAngle(deg),wheelMoveState(unitless),avgBrakePres(unitless),vnYaw(deg),vnPitch(deg),vnRoll(deg),vnAccelX(m/s/s),vnAccelY(m/s/s),vnAccelZ(m/s/s),vnGyroX(rad/s),vnGyroY(rad/s),vnGyroZ(rad/s),gps lat(dd), gps long(dd), rg lat(dd), rg long(dd), vnVelX(m/s), vnVelY(m/s), vnVelZ(m/s)", comments='')
+    dataOutput = np.column_stack((gpsGenPosix,avgSpd,brakeState,wheelOdoRR,speedOBDResponse, wheelSpeedFL,wheelSpeedFR,wheelSpeedRL,wheelSpeedRR,steerWheelAngle,wheelMoveState,avgBrakePres,vnYaw,vnPitch,vnRoll,vnAccelX,vnAccelY,vnAccelZ,vnGyroX,vnGyroY,vnGyroZ,gpsLat,gpsLong,rgLat,rgLong, vnVelX, vnVelY, vnVelZ))
+    np.savetxt(args.output, dataOutput, fmt='%.3f', delimiter=' ', header="# gpsGenPosix(s),avgSpd(mph),brakeState(unitless),wheelodometeryRR(mph),speedOBDResponse(mph),wheelSpeedFL(mph),wheelSpeedFR(mph),wheelSpeedRL(mph),wheelSpeedRR(mph),steerWheelAngle(deg),wheelMoveState(unitless),avgBrakePres(unitless),vnYaw(deg),vnPitch(deg),vnRoll(deg),vnAccelX(m/s/s),vnAccelY(m/s/s),vnAccelZ(m/s/s),vnGyroX(rad/s),vnGyroY(rad/s),vnGyroZ(rad/s),gps lat(dd), gps long(dd), rg lat(dd), rg long(dd), vnVelX(m/s), vnVelY(m/s), vnVelZ(m/s)", comments='')
     print("data has been exported")
     print("")
 
-    plt.subplot(3,1,1)
+
+    # plt.subplot(3,1,1)
     plt.title("relevant vehicle data")
-    plt.plot(gpsGenPosix, wheelSpeedRR, color='gold', label='rr wheel')
-    plt.plot(gpsGenPosix, wheelSpeedRL, color='red', label='rl wheel')
-    plt.plot(gpsGenPosix, wheelSpeedFR, color='blue', label='fr wheel')
-    plt.plot(gpsGenPosix, wheelSpeedFL, color='green', label='fl wheel')
-    plt.plot(gpsGenPosix, wheelMoveState, color='red', label='wheels moving state')
+    plt.figure(1)
+    # plt.plot(gpsGenPosix, wheelSpeedRR, color='gold', label='rr wheel')
+    # plt.plot(gpsGenPosix, wheelSpeedRL, color='red', label='rl wheel')
+    # plt.plot(gpsGenPosix, wheelSpeedFL, color='green', label='fl wheel')
+    # plt.plot(gpsGenPosix, wheelMoveState, color='red', label='wheels moving state')
+    plt.plot(gpsGenPosix, wheelSpeedFR, color='blue', label='fr wheel (CAN)')
+    plt.plot(gpsGenPosix, wheelOdoRR, color='magenta', label='rr direct (Encoder)')
+    plt.plot(gpsGenPosix, speedOBDResponse, color='cyan', label='obd speed response (OBD)')
     plt.ylabel('wheel speed (mph)')
+    plt.xlabel('utc time (s)')
     plt.legend()
     # plt.ylabel('wheels moving state')
 
-    plt.subplot(3,1,2)
-    plt.plot(gpsGenPosix, steerWheelAngle, color='pink')
-    plt.ylabel('steering wheel angle (deg)')
+    # plt.subplot(3,1,2)
+    # plt.plot(gpsGenPosix, steerWheelAngle, color='pink')
+    # plt.ylabel('steering wheel angle (deg)')
 
-    plt.subplot(3,1,3)
-    plt.plot(gpsGenPosix, avgBrakePres, color='yellow')
-    plt.ylabel('avg brake press (unitless)')
-    plt.xlabel('utc time (s)')
+    # plt.subplot(3,1,3)
+    # plt.plot(gpsGenPosix, avgBrakePres, color='yellow')
+    # plt.ylabel('avg brake press (unitless)')
+    # plt.xlabel('utc time (s)')
 
     plt.figure(2)
     plt.title("vectornav / racegrade compare")
