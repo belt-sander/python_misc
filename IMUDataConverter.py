169eaea9406dab8cd1d6199c2fcf497edccfab91
Sander Marques <sander@mapper.ai>
2019-10-14 12:01:59 -0700

    added gpsTime to text output field

--- IMUDataConverter.py
+++ IMUDataConverter.py
@@ -32,9 +32,9 @@ def main():
 	converted_gyro_x = np.zeros((len(inputData),1))
 	converted_gyro_y = np.zeros((len(inputData),1))
 	converted_gyro_z = np.zeros((len(inputData),1))
+	converted_time = np.zeros((len(inputData),1))
 
 	sysTime = inputData[:,0]
-	gpsTime = inputData[:,1]
 
    	for i, row in enumerate(inputData):
 		gyroX = row[4]
@@ -43,6 +43,7 @@ def main():
 		accelX = row[7]
 		accelY = row[8]
 		accelZ = row[9]
+		gpsTime = row[1]
 
 		converted_accel_x[i,: ] = (accelX / 32768)*2
 		converted_accel_y[i,: ] = (accelY / 32768)*2
@@ -50,23 +51,24 @@ def main():
 		converted_gyro_x[i,: ] = (gyroX / 32768) * 2000
 		converted_gyro_y[i,: ] = (gyroY / 32768) * 2000
 		converted_gyro_z[i,: ] = (gyroZ / 32768) * 2000				
+		converted_time[i,: ] = gpsTime / 1e6 # uS to Seconds
 
-	dataOutput = np.column_stack((gpsTime, converted_gyro_x, converted_gyro_y, converted_gyro_z, converted_accel_x, converted_accel_y, converted_accel_z))
-	np.savetxt(args.output_file, dataOutput, fmt='%.8f', delimiter=' ', header="# utcTime, gyro X, gyro Y, gyro Z, accel X, accel Y, accel Z", comments='')
+	dataOutput = np.column_stack((converted_time, converted_gyro_x, converted_gyro_y, converted_gyro_z, converted_accel_x, converted_accel_y, converted_accel_z))
+	np.savetxt(args.output_file, dataOutput, fmt='%.8f', delimiter=' ', header="# utcTime (sec), gyro X (deg/sec), gyro Y (deg/sec), gyro Z (deg/sec), accel X (G), accel Y (G), accel Z (G)", comments='')
 	print("data has been exported")
 	print("")
 
 	plt.figure(1)
 	plt.subplot(211)
-	plt.plot(gpsTime, converted_accel_x, label="accel x (G)")
-	plt.plot(gpsTime, converted_accel_y, label="accel y (G)")
-	plt.plot(gpsTime, converted_accel_z, label="accel z (G)")
+	plt.plot(converted_time, converted_accel_x, label="accel x (G)")
+	plt.plot(converted_time, converted_accel_y, label="accel y (G)")
+	plt.plot(converted_time, converted_accel_z, label="accel z (G)")
 	plt.legend()
 
 	plt.subplot(212)
-	plt.plot(gpsTime, converted_gyro_x, label="gyro x")
-	plt.plot(gpsTime, converted_gyro_y, label="gyro y")
-	plt.plot(gpsTime, converted_gyro_z, label="gyro z")
+	plt.plot(converted_time, converted_gyro_x, label="gyro x")
+	plt.plot(converted_time, converted_gyro_y, label="gyro y")
+	plt.plot(converted_time, converted_gyro_z, label="gyro z")
 	plt.legend()
 
 	plt.show()
