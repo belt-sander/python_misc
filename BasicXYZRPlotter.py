b87a73cd799f12f883444cda967a05c33c906f91
Sander Marques <sander@mapper.ai>
2019-10-24 08:26:05 -0700

    basic functional xyzr plotter with pyQt

--- BasicXYZRPlotter.py
+++ BasicXYZRPlotter.py
@@ -2,26 +2,20 @@
 
 import argparse
 import numpy as np
-import datetime
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
 
 def parse_args():
 	arg_parser = argparse.ArgumentParser(description='simple visualization tool for xyzr data')
-	arg_parser.add_argument('-r', '--rows', required=True, type=int, help='number of rows to process')
+	arg_parser.add_argument('-r', '--rows', required=False, default=1000000, type=int, help='number of rows to process, default is 1000000')
 	arg_parser.add_argument('-s1', '--scan_1', required=True, help='xyzr scan 1')
-	arg_parser.add_argument('-s2', '--scan_2', required=False, help='xyzr scan 2')
-	arg_parser.add_argument('-s3', '--scan_3', required=False, help='xyzr scan 3')
-	arg_parser.add_argument('-s4', '--scan_4', required=False, help='xyzr scan 4')
 	return arg_parser.parse_args()
 
 def main():
 	args = parse_args()
 	
 	scan1 = np.genfromtxt(args.scan_1)
-	# scan2 = np.genfromtxt(args.scan_2)
-	# scan3 = np.genfromtxt(args.scan_3)
-	# scan4 = np.genfromtxt(args.scan_4)
+
 	print("")
 	print("data has been imported")
 	print("")
@@ -31,6 +25,9 @@ def main():
 	y1_sub = np.zeros((len(scan1),1))
 	z1_sub = np.zeros((len(scan1),1))
 
+	x_y_z_sub = np.zeros((len(scan1),3))
+	print('xyz array size: ', np.size(x_y_z_sub))
+
 	for i, row in enumerate(scan1):
 		x1 = row[0]
 		y1 = row[1]
@@ -38,16 +35,33 @@ def main():
 
 		x1_sub[i,:] = x1
 		y1_sub[i,:] = y1
-		z1_sub[i,:] = z1			
+		z1_sub[i,:] = z1 + 2
+
+		# x_y_z_sub[i,:] = x1 + y1 + z1
+		# print('print xyz: ', x_y_z_sub)			
 
 		if i > args.rows:
 			break
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
-	ax.scatter(x1_sub, y1_sub, z1_sub)
+	x_y_z_sub = np.concatenate((x1_sub, y1_sub, z1_sub), axis=1)
 
-	plt.show()
+	# open app
+	app=pg.QtGui.QApplication([])
+
+	# array of data
+	plt = gl.GLScatterPlotItem(pos=x_y_z_sub, color=[1.0,0.5,0.5,0.5])
+
+	# add widget?	
+	w = gl.GLViewWidget()
+	w.addItem(plt)
+
+	# add grid
+	g = gl.GLGridItem()
+	w.addItem(g)
+	
+	# show
+	w.show()
+	pg.QtGui.QApplication.exec_()
 
 if __name__=='__main__':
 	main()
