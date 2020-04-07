105132b9b6c63c74f27f4befbd5e7fe5f6dce6b4
Sander <sander@obsidianeng.com>
2020-04-07 14:50:46 -0400

    Corrected rotations!

--- BasicXYZRPlotter.py
+++ BasicXYZRPlotter.py
@@ -24,6 +24,11 @@ def main():
 	y1 = np.zeros((len(scan1),1))
 	z1 = np.zeros((len(scan1),1))
 	r1 = np.zeros((len(scan1),1))
+
+	x1_rot = np.zeros((len(scan1),1))
+	y1_rot = np.zeros((len(scan1),1))
+	z1_rot = np.zeros((len(scan1),1))	
+	
 	one1 = np.zeros((len(scan1),1))
 	zero1 = np.zeros((len(scan1),1))
 
@@ -39,64 +44,55 @@ def main():
 		r1[i,:] = _r1 / 255.0
 		one1[i,:] = 1.0
 
-		_point_array = np.array([[_z1],[_x1],[_y1]]) # yaw, pitch, roll
-		_rotation = R.from_euler('zyx', [args.yaw, args.pitch, args.roll], degrees=True)
-		_rotation_matrix = _rotation.as_matrix()
-		_rotated_output = np.matmul(_rotation_matrix, _point_array)
+		### rotation tests ###
+		'''
+		reference frame of lidar (velarray) is:
+		x == right
+		y == forward
+		z == up
+		'''
+
+		_point_array = np.array([[_x1],[_y1],[_z1]]) # yaw, pitch, roll		
+		_rotation = R.from_euler('zxy', [args.yaw, args.pitch, args.roll], degrees=True)		
+		_rotation_matrix = _rotation.as_matrix()	
+		_rotated_output = np.matmul(_rotation_matrix, _point_array)		
 		_rotated_point_array = np.reshape(_rotated_output, len(_rotated_output))
 
-		print('output: ',np.reshape(_point_array, len(_point_array)), _rotated_point_array)
-
+		z1_rot[i,:] = _rotated_point_array[2] + 2 ### z height offset
+		x1_rot[i,:] = _rotated_point_array[0]
+		y1_rot[i,:] = _rotated_point_array[1]
 
 		if i > args.rows:
 			break
 
+	pre_rotation_data = np.column_stack((x1, y1, z1))
 
-	### rotation tests ###
-	'''
-	reference frame of lidar (velarray) is:
-	x == right
-	y == forward
-	z == up
-	'''
-	# rows = np.zeros((len(scan1),1))
-	# x1_rot = np.zeros((len(scan1),1))
-	# y1_rot = np.zeros((len(scan1),1))
-	# z1_rot = np.zeros((len(scan1),1))
-	# pre_rotation_data = np.column_stack((x1, y1, z1))
-	# for i, row in enumerate(pre_rotation_data):
-	# 	rows[i,:] = i
-	# 	_x1 = row[0]
-	# 	_y1 = row[1]
-	# 	_z1 = row[2]
+	### concatanate scan daterz ###
+	x_y_z_1 = np.concatenate((x1, y1, z1), axis=1)
+	reflectivity_1 = np.concatenate((one1, zero1, zero1, r1), axis=1)
 
+	x_y_z_1_rot = np.concatenate((x1_rot, y1_rot, z1_rot), axis=1) # rotation test
+	reflectivity_1_rot = np.concatenate((zero1, one1, zero1, r1), axis=1) # rotation test
 
-	# ### concatanate scan daterz ###
-	# x_y_z_1 = np.concatenate((x1, y1, z1), axis=1)
-	# reflectivity_1 = np.concatenate((one1, zero1, zero1, r1), axis=1)
+	# open app
+	app = pg.QtGui.QApplication([])
 
-	# x_y_z_1_rot = np.concatenate((x1_rot, y1_rot, z1_rot), axis=1) # rotation test
-	# reflectivity_1_rot = np.concatenate((zero1, one1, zero1, r1), axis=1) # rotation test
+	# array of data
+	plt_1 = gl.GLScatterPlotItem(pos=x_y_z_1, color=reflectivity_1, size=4)
+	plt_1_rot = gl.GLScatterPlotItem(pos=x_y_z_1_rot, color=reflectivity_1_rot, size=4)
 
-	# # open app
-	# app = pg.QtGui.QApplication([])
+	# add widget / plot data	
+	w = gl.GLViewWidget()
+	w.addItem(plt_1)
+	w.addItem(plt_1_rot)
 
-	# # array of data
-	# plt_1 = gl.GLScatterPlotItem(pos=x_y_z_1, color=reflectivity_1, size=4)
-	# plt_1_rot = gl.GLScatterPlotItem(pos=x_y_z_1_rot, color=reflectivity_1_rot, size=4)
-
-	# # add widget / plot data	
-	# w = gl.GLViewWidget()
-	# w.addItem(plt_1)
-	# w.addItem(plt_1_rot)
-
-	# # add grid
-	# g = gl.GLGridItem()
-	# w.addItem(g)
+	# add grid
+	g = gl.GLGridItem()
+	w.addItem(g)
 	
-	# # show
-	# w.show()
-	# pg.QtGui.QApplication.exec_()
+	# show
+	w.show()
+	pg.QtGui.QApplication.exec_()
 
 if __name__=='__main__':
 	main()
